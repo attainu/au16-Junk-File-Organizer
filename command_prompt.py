dbb37192c997390f2bfe2a34ee3184a8d3460ef5
Akash Verma <akashcool2014.av@gmail.com>
2021-02-17 21:34:12 +0530

    version - 3 : completed

--- command_prompt.py
+++ command_prompt.py
@@ -7,17 +7,16 @@ def run(args):
     OrganiseMethod(args.d, args.o)
     return 'Finish'
 
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    
-    parser.add_argument('-d', type = str, default = None,
-                        help = "Give the directory address where you want to run the junk_file_organiser")
 
-    parser.add_argument('-o', type = str, default = None,
-                        help = "Give the organising method")
+    comment_one = "Give the directory address where you want to run the"
+    comment_one += "junk_file_organiser"
+    parser.add_argument('-d', type=str, default=None, help=comment_one)
+
+    comment_two = "Give the organising method"
+    parser.add_argument('-o', type=str, default=None, help=comment_two)
 
     args = parser.parse_args()
-    
     sys.stdout.write(run(args))
-
-# python command_prompt.py -d "C:\Users\Vibhor Verma\Desktop\testing" -o "extension"
